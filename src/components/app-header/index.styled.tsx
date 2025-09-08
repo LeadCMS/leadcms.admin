7be07923989928410cd3dc1e91b5ca15e8519fd2
M-lakshan <emlakshan92@gmail.com>
2025-09-08 11:15:14 +0530

    customized styles moved to the styling file + meterial UI 'Menu' replaced with customized 'AccountMenu'

--- src/components/app-header/index.styled.tsx
+++ src/components/app-header/index.styled.tsx
@@ -1,4 +1,4 @@
-import { AppBar, styled, Toolbar } from "@mui/material";
+import { AppBar, Menu, MenuProps, styled, Toolbar } from "@mui/material";
 
 export const AppBarStyled = styled(AppBar)`
   position: sticky;
@@ -13,6 +13,37 @@ export const AppBarStyled = styled(AppBar)`
   display: flex;
   justify-content: center;
 `;
+
+export const AccountMenu = styled((props: MenuProps) => <Menu {...props} />)(({ theme }) => ({
+  "& .MuiPaper-root": {
+    borderRadius: theme.shape.borderRadius,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    boxShadow: "rgb(0 0 0 / 20%) 0px 2px 10px",
+
+    "& .MuiDivider-root": {
+      margin: "0 !important",
+    },
+    "& .MuiList-root": {
+      padding: "0 !important",
+    },
+    "& .MuiMenuItem-root": {
+      padding: "8px 16px",
+
+      "&:hover": {
+        backgroundColor: "rgba(0, 0, 0, 0.05)",
+      },
+
+      "&:first-of-type": {
+        padding: "10px 16px 8px",
+      },
+
+      "&:last-of-type": {
+        padding: "8px 16px 10px",
+      },
+    },
+  },
+}));
 
 export const AppBarToolbar = styled(Toolbar)`
   min-height: 64px;
