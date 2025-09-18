8e7f924d2d1f72c0f0e65cbca765c728c7c40ee6
M-lakshan <emlakshan92@gmail.com>
2025-09-18 08:09:44 +0530

    minor updates + 'active' tabular state initiated

--- src/components/tabular-grid/index.tsx
+++ src/components/tabular-grid/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { LocalContainerProps } from "types";
 import { SetComponentStyles } from "@utils/general-helper";
 import { Root, List, Trigger, Content } from "@radix-ui/react-tabs";
@@ -18,91 +19,174 @@ export const TabularGrid = ({
   cmpID,
   styleObj,
   gridObj,
+  className,
+  tableName,
 }: LocalContainerProps & {
   gridObj: any;
+  tableName?: string;
 }) => {
-  const cmpStyles = SetComponentStyles({ styleObj });
+  const cmpStyles = SetComponentStyles({ className, styleObj });
+  const [activeTab, setActiveTab] = useState({
+    idx: 0,
+    label: Object.keys(gridObj)[0],
+  });
+
+  const getTableTabClassList = ({ keyIdx, tabLabel }: { keyIdx: number; tabLabel: string }) => {
+    return [
+      keyIdx == Object.keys(gridObj).length - 1 ? "last" : "",
+      keyIdx != Object.keys(gridObj).length - 1 &&
+      keyIdx != activeTab.idx &&
+      keyIdx == activeTab.idx - 1
+        ? "pre-neighbour"
+        : "",
+      tabLabel == activeTab.label ? "active" : "",
+      keyIdx != 0 && keyIdx != activeTab.idx && keyIdx == activeTab.idx + 1
+        ? "post-neighbour psd-after-effects"
+        : "",
+    ].join(" ");
+  };
+
+  const handleTriggerHoverIn = (hoveredEl: HTMLElement) => {
+    const triggers = document.querySelectorAll(".controller-btn-tab");
+
+    triggers.forEach((el) => {
+      if (
+        el !== hoveredEl &&
+        !hoveredEl.classList.contains("active") &&
+        Array.from(hoveredEl.classList).some((cls) =>
+          ["pre-neighbour", "post-neighbour"].includes(cls)
+        )
+      ) {
+        triggers.forEach((elAlt) => {
+          const classes = elAlt.className
+            .split(" ")
+            .map((clsAlt) => (clsAlt === "psd-after-effects" ? "psd-after" : clsAlt));
+
+          elAlt.className = classes.join(" ");
+        });
+      }
+    });
+  };
+
+  const handleTriggerHoverOut = () => {
+    const triggers = document.querySelectorAll(".controller-btn-tab");
+
+    triggers.forEach((el) => {
+      const classes = el.className
+        .split(" ")
+        .map((cls) => (cls === "psd-after" ? "psd-after-effects" : cls));
+
+      el.className = classes.join(" ");
+    });
+  };
 
   return (
     <div {...(cmpID && { id: cmpID })} {...(cmpStyles && { className: cmpStyles })}>
-      <Root defaultValue="status" className="tab-grid">
+      <Root
+        defaultValue={activeTab.label}
+        onValueChange={(e) =>
+          setActiveTab({
+            idx: Object.keys(gridObj).indexOf(e),
+            label: e,
+          })
+        }
+        className={`tab-grid${tableName ? "" : " no-table-name"}`}
+      >
+        {tableName && <h2 className="table-name">{tableName}</h2>}
         <List className="tab-controller">
-          {Object.keys(gridObj).map((tab, tabKey) => (
-            <Trigger key={tabKey} value={tab}>
-              {gridObj[tab]["identifier"]}
-            </Trigger>
-          ))}
+          <div className="tab-controller-btns">
+            {Object.keys(gridObj).map((tabValue, tabKey) => (
+              <Trigger
+                key={tabKey}
+                value={tabValue}
+                className={`tab controller-btn-tab ${getTableTabClassList({
+                  keyIdx: tabKey,
+                  tabLabel: tabValue,
+                })}`}
+                onMouseEnter={(e) => handleTriggerHoverIn(e.currentTarget)}
+                onMouseLeave={handleTriggerHoverOut}
+              >
+                <>{gridObj[tabValue]["identifier"]}</>
+              </Trigger>
+            ))}
+          </div>
         </List>
 
-        <Content value="status" className="tab-content status-tab-expand">
-          <>
-            <h2 className="title">{gridObj.status.type}</h2>
-            <ProgressBar
-              rate={gridObj.status.healthProgress}
-              color="#4CAF50"
-              label="Overall Health"
-              value={true}
-              negation={true}
-              negColor="#283329"
-            />
-            <div className="info">
-              {gridObj.status.services.map((service: any) => {
-                const Icon = iconMap[service.icon as keyof typeof iconMap];
+        {activeTab.label === "status" && (
+          <Content value="status" className="tab-content status-tab-expand">
+            <>
+              {/* <h2 className="tab-title">{gridObj.status.type}</h2> */}
+              <ProgressBar
+                rate={gridObj.status.healthProgress}
+                label="Overall Health"
+                value={true}
+                negation={true}
+              />
+              <div className="detail-container">
+                {gridObj.status.services.map((service: any) => {
+                  const Icon = iconMap[service.icon as keyof typeof iconMap];
 
-                return (
-                  <div key={service.name} className={`service ${service.bgColor}`}>
-                    {Icon && <Icon className={`${service.statusColor} h-6 w-6`} />}
-                    <div>
-                      <p className="service_name">{service.name}</p>
-                      <p className="service_info">{service.description}</p>
+                  return (
+                    <div key={service.name} className={`service ${service.bgColor}`}>
+                      {Icon && <Icon className={`${service.statusColor}`} />}
+                      <div className="details">
+                        <p className="name">{service.name}</p>
+                        <p className="descrp">{service.description}</p>
+                      </div>
                     </div>
-                  </div>
+                  );
+                })}
+              </div>
+            </>
+          </Content>
+        )}
+
+        {activeTab.label === "database" && (
+          <Content value="database" className="tab-content database-tab-expand">
+            {/* <h2 className="tab-title">{gridObj.status.type}</h2> */}
+            <div className="list">
+              {Object.entries(gridObj.database).map(([infoKey, value]) => (
+                <p key={infoKey} className={"database-meta-info"}>
+                  <span>{infoKey}</span>
+                  <strong>{String(value)}</strong>
+                </p>
+              ))}
+            </div>
+          </Content>
+        )}
+
+        {activeTab.label === "deployement" && (
+          <Content value="deployement" className="tab-content deployement-tab-expand">
+            {/* <h2 className="tab-title">{gridObj.status.type}</h2> */}
+            <ul className="list">
+              {Object.entries(gridObj.deployement).map(([infoKey, value]) => {
+                if (infoKey === "dockerHelp") return null;
+                if (infoKey === "showDockerHelp") return null;
+                return (
+                  <p key={infoKey} className={"database-meta-info"}>
+                    <span>{infoKey}</span>
+                    <strong>{String(value)}</strong>
+                  </p>
                 );
               })}
-            </div>
-          </>
-        </Content>
+            </ul>
 
-        <Content value="database" className="tab-content database-tab-expand">
-          <h2 className="title">Database Info</h2>
-          <ul className="list">
-            {Object.entries(gridObj.database).map(([infoKey, value]) => (
-              <li key={infoKey}>
-                <strong>{infoKey}:</strong> {String(value)}
-              </li>
-            ))}
-          </ul>
-        </Content>
-
-        <Content value="deployement" className="tab-content deployement-tab-expand">
-          <h2 className="title">Deployment Info</h2>
-          <ul className="list">
-            {Object.entries(gridObj.deployement).map(([infoKey, value]) => {
-              if (infoKey === "dockerHelp") return null;
-              if (infoKey === "showDockerHelp") return null;
-              return (
-                <li key={infoKey}>
-                  <strong>{infoKey}:</strong> {String(value)}
-                </li>
-              );
-            })}
-          </ul>
-
-          {gridObj.deployement.showDockerHelp && (
-            <div className="sub-tab-content">
-              <p className="sub-title">{gridObj.deployement.dockerHelp.helpText}</p>
-              <TerminalContainer
-                styleObj={{
-                  cmpTag: "terminal",
-                  cmpStyles: ["container"],
-                }}
-                cliObj={{
-                  cmd: gridObj.deployement.dockerHelp.commands,
-                }}
-              />
-            </div>
-          )}
-        </Content>
+            {gridObj.deployement.showDockerHelp && (
+              <div className="sub-tab-content development-terminal-container">
+                <p className="sub-title">{gridObj.deployement.dockerHelp.helpText}</p>
+                <TerminalContainer
+                  styleObj={{
+                    cmpTag: "container",
+                    cmpStyles: ["terminal-container", "development-terminal"],
+                  }}
+                  cliObj={{
+                    cmd: gridObj.deployement.dockerHelp.commands,
+                  }}
+                />
+              </div>
+            )}
+          </Content>
+        )}
       </Root>
     </div>
   );
