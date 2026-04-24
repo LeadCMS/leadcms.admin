6798802929fce7b38d3ee41aa14fa50ddd38f8ec
Vladislav Seregin <vd.seregin@gmail.com>
2026-04-24 08:12:18 +0200

    fix: fix routers definition

--- src/features/unsubscribes/index.tsx
+++ src/features/unsubscribes/index.tsx
@@ -1,4 +1,4 @@
-import { addFormRoute, editFormRoute, viewFormRoute } from "@lib/router";
+import { getAddFormRoute, getEditFormRoute, getViewFormRoute } from "@lib/router";
 import { Outlet, Route, Routes } from "react-router-dom";
 import { UnsubscribesList } from "./list";
 import { UnsubscribeFormPage } from "./form";
@@ -8,9 +8,9 @@ export const UnsubscribesModule = () => {
     <>
       <Routes>
         <Route index element={<UnsubscribesList />} />
-        <Route path={editFormRoute.template} element={<UnsubscribeFormPage mode="edit" />} />
-        <Route path={viewFormRoute.template} element={<UnsubscribeFormPage mode="view" />} />
-        <Route path={addFormRoute.template} element={<UnsubscribeFormPage mode="create" />} />
+        <Route path={getEditFormRoute()} element={<UnsubscribeFormPage mode="edit" />} />
+        <Route path={getViewFormRoute()} element={<UnsubscribeFormPage mode="view" />} />
+        <Route path={getAddFormRoute()} element={<UnsubscribeFormPage mode="create" />} />
       </Routes>
       <Outlet />
     </>
